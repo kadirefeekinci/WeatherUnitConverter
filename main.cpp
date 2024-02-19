6298423c17da3f993bd9f740a130941f83052d53
Efe <kadirefeekinci@gmail.com>
2024-02-19 17:35:00 +0300

    Update main.cpp
    "goto menu" added

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main() {
 
     char Operator;
     double a;
-    cout<<"Select the operator you want to use (F for Fahrenheit to Celsius, C for Celsius to Fahrenheit): ";
+    menu: cout<<"Select the operator you want to use (F for Fahrenheit to Celsius, C for Celsius to Fahrenheit): ";
     cin>>Operator;
 
     switch (Operator) {
@@ -32,7 +32,8 @@ int main() {
             CelToFah(a);
             break;
         default:
-            cout<<"nonvalid operator";
+            cout<<"nonvalid operator"<<endl;
+            goto menu;
 }
     return 0;
 }
